2934871ba5c9b5902d8d973746745d28f55eb2f0
chetan <chetanraval703@gmail.com>
2024-11-19 00:22:35 +0530

    3rd Commit - Change in Home Page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+// import { Button } from "@/components/ui/button";
+
+// import Image from "next/image";
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <h2>Hello I am Chetan</h2>
+//       <Button>Subscribe</Button>
+//     </div>
+
+//   );
+// }
+
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div>
-      <h2>Hello I am Chetan</h2>
-      <Button>Subscribe</Button>
-    </div>
-  );
+  redirect('/dashboard'); // Redirect to the sign-up page
 }
